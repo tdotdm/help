1b9d6ed69b125304f57680dfab3bf68bf27ce463
Darryn McGaw <dm.maxton@gmail.com>
2020-02-24 23:51:21 +0000

    Create SetHelp

---
+++ src/main/java/com/github/tdotdm/SetHelp.java
@@ -0,0 +1,11 @@
+package com.github.tdotdm;
+
+import java.util.Set;
+
+public class SetHelp extends Help {
+    private final Set<?> value;
+
+    public SetHelp(final Set<?> value) {
+        this.value = value;
+    }
+}
