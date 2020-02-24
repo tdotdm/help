39826b1e2590247e4513ced3112166458c6f0294
Darryn McGaw <dm.maxton@gmail.com>
2020-02-24 23:49:03 +0000

    Add help_ShouldReturnStringHelp_WhenGivenString to HelpTest

--- src/test/java/com/github/tdotdm/HelpTest.java
+++ src/test/java/com/github/tdotdm/HelpTest.java
@@ -1,4 +1,16 @@
 package com.github.tdotdm;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HelpTest {
+    @Test
+    public void help_ShouldReturnStringHelp_WhenGivenString() {
+        //when
+        final StringHelp result = Help.help("string");
+
+        //then
+        assertThat(result).isNotNull();
+    }
 }
