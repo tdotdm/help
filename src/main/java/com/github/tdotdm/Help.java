f4de7222fa3d4016a086a11de46a0ef07eb558de
Darryn McGaw <dm.maxton@gmail.com>
2020-02-24 23:28:32 +0000

    Add protected validate method to Help

--- src/main/java/com/github/tdotdm/Help.java
+++ src/main/java/com/github/tdotdm/Help.java
@@ -4,4 +4,12 @@ public abstract class Help {
     public static StringHelp help(final String value) {
         return new StringHelp(value);
     }
+
+    protected void validate(final Object... objects) {
+        for (Object object : objects) {
+            if (object == null) {
+                throw new HelpException("Cannot create Help with illegal arguments!");
+            }
+        }
+    }
 }
