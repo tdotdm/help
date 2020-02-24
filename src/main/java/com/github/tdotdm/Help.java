d7b470768550206412ba7aad903b038a5ee4f65f
Darryn McGaw <dm.maxton@gmail.com>
2020-02-24 23:14:00 +0000

    Rename with method to help in Help

--- src/main/java/com/github/tdotdm/Help.java
+++ src/main/java/com/github/tdotdm/Help.java
@@ -1,7 +1,7 @@
 package com.github.tdotdm;
 
 public abstract class Help {
-    public static StringHelp with(final String value) {
+    public static StringHelp help(final String value) {
         return new StringHelp(value);
     }
 }
