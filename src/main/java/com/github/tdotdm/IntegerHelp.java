e6247fdd03fdfa7017fa4a18bc573031626c8dec
Darryn McGaw <dm.maxton@gmail.com>
2020-02-25 22:03:02 +0000

    Add byCheckingNotExceedingBoundary method to IntegerHelp

--- src/main/java/com/github/tdotdm/IntegerHelp.java
+++ src/main/java/com/github/tdotdm/IntegerHelp.java
@@ -7,4 +7,13 @@ public class IntegerHelp extends Help {
         validate(value);
         this.value = value;
     }
+
+    public boolean byCheckingNotExceedingBoundary(final Integer lowerBoundary,
+                                                  final Integer upperBoundary) {
+        if ((lowerBoundary.equals(upperBoundary)) || lowerBoundary > upperBoundary) {
+            throw new HelpException("Lower is greater than Upper!");
+        }
+
+        return this.value >= lowerBoundary && this.value <= upperBoundary;
+    }
 }
