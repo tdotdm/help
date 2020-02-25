42afb81d8dae00847813113ddd433cd88cfcabdc
Darryn McGaw <dm.maxton@gmail.com>
2020-02-25 00:06:57 +0000

    Add byRemovingDuplicates to ListHelp

--- src/main/java/com/github/tdotdm/ListHelp.java
+++ src/main/java/com/github/tdotdm/ListHelp.java
@@ -1,6 +1,9 @@
 package com.github.tdotdm;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ListHelp extends Help {
     private final List<?> value;
@@ -8,5 +11,11 @@ public class ListHelp extends Help {
     public ListHelp(final List<?> value) {
         validate(value);
         this.value = value;
+    }
+
+    public List<?> byRemovingDuplicates() {
+        final Set<?> valueSet = new HashSet<>(value);
+
+        return new ArrayList<>(valueSet);
     }
 }
