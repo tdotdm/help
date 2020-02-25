8059b347187e1f7033e4f661a272af53215ed9f2
Darryn McGaw <dm.maxton@gmail.com>
2020-02-25 22:03:15 +0000

    Add tests to IntegerHelpTest

--- src/test/java/com/github/tdotdm/IntegerHelpTest.java
+++ src/test/java/com/github/tdotdm/IntegerHelpTest.java
@@ -2,6 +2,7 @@ package com.github.tdotdm;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class IntegerHelpTest {
@@ -10,5 +11,39 @@ public class IntegerHelpTest {
         //then
         assertThatThrownBy(() -> new IntegerHelp(null))
                 .isInstanceOf(HelpException.class);
+    }
+
+    @Test
+    public void byCheckingNotExceedingBoundary_ShouldThrowException_WhenLowerIsGreaterThanUpper() {
+        //given
+        final IntegerHelp target = new IntegerHelp(5);
+
+        //then
+        assertThatThrownBy(() -> target.byCheckingNotExceedingBoundary(2, 1))
+                .isInstanceOf(HelpException.class);
+    }
+
+    @Test
+    public void byCheckingNotExceedingBoundary_ShouldReturnTrue_WhenValueIsInBoundary() {
+        //given
+        final IntegerHelp target = new IntegerHelp(5);
+
+        //when
+        final boolean result = target.byCheckingNotExceedingBoundary(1, 10);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void byCheckingNotExceedingBoundary_ShouldReturnFalse_WhenValueIsOutBoundary() {
+        //given
+        final IntegerHelp target = new IntegerHelp(-5);
+
+        //when
+        final boolean result = target.byCheckingNotExceedingBoundary(1, 10);
+
+        //then
+        assertThat(result).isFalse();
     }
 }
