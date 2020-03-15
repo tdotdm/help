fe158cfc254e7cb070ff91c0ceca2b6e13874b78
Darryn McGaw <dm.maxton@gmail.com>
2020-03-15 22:11:03 +0000

    Create and implement BooleanHelpTest

--- src/main/java/com/github/tdotdm/BooleanHelp.java
+++ src/main/java/com/github/tdotdm/BooleanHelp.java
@@ -9,4 +9,23 @@ public final class BooleanHelp extends Help<Boolean> {
     public BooleanHelp(final Boolean value) {
         super(value);
     }
+
+    public String byReturning(final String valueWhenTrue,
+                              final String valueWhenFalse) {
+        validate(valueWhenTrue, "Illegal 'valueWhenTrue'.");
+        validate(valueWhenFalse, "Illegal 'valueWhenFalse'.");
+
+        if (value) {
+            return valueWhenTrue;
+        } else {
+            return valueWhenFalse;
+        }
+    }
+
+    private static void validate(final String target,
+                                 final String errorMessage) {
+        if (target == null) {
+            throw new HelpException(errorMessage);
+        }
+    }
 }
