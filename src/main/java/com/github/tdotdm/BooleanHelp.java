7a5ccced57a3ed30eb567acde6014620d59fb938
Darryn McGaw <dm.maxton@gmail.com>
2020-03-15 21:54:34 +0000

    Create new BooleanHelp with no methods

---
+++ src/main/java/com/github/tdotdm/BooleanHelp.java
@@ -0,0 +1,12 @@
+package com.github.tdotdm;
+
+/*
+ * Class storing multiple
+ * utility methods relating to
+ * Boolean.
+ * */
+public final class BooleanHelp extends Help<Boolean> {
+    public BooleanHelp(final Boolean value) {
+        super(value);
+    }
+}
